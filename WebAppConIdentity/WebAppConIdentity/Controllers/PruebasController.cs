df6ca0db9b1cba8657a4bc3a041bcf6a601475f1
Gerardo G <g.garciac@outlook.com>
2019-08-29 15:16:34 -0500

    Codigo inicial

---
+++ WebAppConIdentity/WebAppConIdentity/Controllers/PruebasController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppConIdentity.Controllers
+{
+    public class PruebasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Informacion", "Esta es la página Principal");
+        }
+
+         public IActionResult Funcion1()
+        {
+            return View("Informacion", "Esta es una función sin acceso restringido");
+        }
+
+        [Authorize]
+        public IActionResult Funcion2()
+        {
+            return View("Informacion", $"Esta es una función con acceso restringido. Está siendo accedida por {User?.Identity?.Name}");
+        }
+    }
+}
---
+++ WebAppConIdentity/WebAppConIdentity/Data/Migrations/20190829193446_ModeloInicialIdentity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppConIdentity.Data.Migrations
+{
+    public partial class ModeloInicialIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
